c8b9b40bf6bf0717fb54b5c0516863fceda5ca0b
Matthew James Wong <matthew.wong76@gmail.com>
2020-06-21 20:59:22 -0600

    form inputs are now parsed and added to the url

--- app/src/main/java/com/example/ebaycatalogsearch/MainActivity.java
+++ app/src/main/java/com/example/ebaycatalogsearch/MainActivity.java
@@ -12,10 +12,22 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     // Form Fields
-    private EditText keyword;
+    private EditText keywords;
     private EditText priceFrom;
     private EditText priceTo;
     private CheckBox conditionNew;
@@ -27,6 +39,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     private TextView keywordWarning;
     private TextView priceRangeWarning;
 
+    // Request Object
+    private RequestQueue requestQueue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
@@ -34,7 +49,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         setContentView(R.layout.activity_main);
 
         // Initialize the Form fields
-        keyword = findViewById(R.id.keyword);
+        keywords = findViewById(R.id.keyword);
         priceFrom = findViewById(R.id.priceFrom);
         priceTo = findViewById(R.id.priceTo);
         conditionNew = findViewById(R.id.conditionNew);
@@ -54,6 +69,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         keywordWarning = findViewById(R.id.keywordsWarning);
         priceRangeWarning = findViewById(R.id.priceRangeWarning);
 
+        // Initialize the Request Queue
+        requestQueue = Volley.newRequestQueue(this);
     }
 
     @Override
@@ -71,8 +88,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // boolean to check if any fields are invalid
         boolean invalidFields = false;
 
-        // Check to make sure keyword field is NOT empty
-        String keywordString = keyword.getText().toString();
+        // Check to make sure keywords field is NOT empty
+        String keywordString = keywords.getText().toString();
 
         if (keywordString == null || keywordString.length() == 0) {
             keywordWarning.setVisibility(View.VISIBLE);
@@ -102,6 +119,9 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         if (invalidFields) {
             Toast.makeText(MainActivity.this, "Please fix all fields with errors",
                     Toast.LENGTH_SHORT).show();
+        } else {
+            // Make the HTTP Request
+            getItems();
         }
     }
 
@@ -112,7 +132,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         priceRangeWarning.setVisibility(View.GONE);
 
         // Clear/Reset ALL fields
-        keyword.setText("");
+        keywords.setText("");
 
         priceFrom.setText("");
         priceTo.setText("");
@@ -123,5 +143,64 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         sortBy.setSelection(0);
 
+    }
+
+    private void getItems() {
+        String url = "https://mjwong-csci-571-hw8.wl.r.appspot.com/items?";
+
+        // Add the Keywords
+        url += "keywords=" + keywords.getText().toString();
+
+        // Add the other parameters
+        if (!priceFrom.getText().toString().equals("")) {
+            url += "&priceFrom=" + priceFrom.getText().toString();
+        }
+
+        if (!priceTo.getText().toString().equals("")) {
+            url += "&priceTo=" + priceTo.getText().toString();
+        }
+
+        if (conditionNew.isChecked()) {
+            url += "&conditionNew=true";
+        }
+
+        if (conditionUsed.isChecked()) {
+            url += "&conditionUsed=true";
+        }
+
+        if (conditionUnspecified.isChecked()) {
+            url += "&conditionUnspecified=true";
+        }
+
+        // Add the sortBy field
+        String sortByValue = "";
+
+        if (sortBy.getSelectedItem().toString().equals("Best Match")) {
+            sortByValue = "BestMatch";
+        } else if (sortBy.getSelectedItem().toString().equals("Price: Highest first")) {
+            sortByValue = "CurrentPriceHighest";
+        } else if (sortBy.getSelectedItem().toString().equals("Price + Shipping: Highest first")) {
+            sortByValue = "PricePlusShippingHighest";
+        } else if (sortBy.getSelectedItem().toString().equals("Price + Shipping: Lowest first")) {
+            sortByValue = "PricePlusShippingLowest";
+        }
+
+        url += "&sortBy=" + sortByValue;
+
+        System.out.println(url);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(request);
     }
 }
