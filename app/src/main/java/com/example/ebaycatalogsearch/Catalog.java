0f5edef7419ebe5cf5e9b720a1ff480e868988e3
Matthew James Wong <matthew.wong76@gmail.com>
2020-06-22 22:02:31 -0600

    item count display has color now

--- app/src/main/java/com/example/ebaycatalogsearch/Catalog.java
+++ app/src/main/java/com/example/ebaycatalogsearch/Catalog.java
@@ -3,7 +3,11 @@ package com.example.ebaycatalogsearch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -51,8 +55,29 @@ public class Catalog extends AppCompatActivity {
         }
 
         TextView itemCountDisplay = findViewById(R.id.itemCountDisplay);
-        itemCountDisplay.setText("Showing " + itemCount + " results for " + requestKeywords);
+        itemCountDisplay.setText(createItemCountDisplay());
 
 
     }
+
+    private SpannableString createItemCountDisplay() {
+        String rawItemCountDisplayString = "Showing " + itemCount + " results for " + requestKeywords;
+
+        int itemCountStartIndex = 8;
+        int itemCountEndIndex = itemCountStartIndex + String.valueOf(itemCount).length();
+
+        int requestKeywordsStartIndex = itemCountStartIndex + String.valueOf(itemCount).length() + 13;
+        int requestKeywordsEndIndex = rawItemCountDisplayString.length();
+
+        SpannableString itemCountDisplayString = new SpannableString(rawItemCountDisplayString);
+
+        ForegroundColorSpan fcsBlue1 = new ForegroundColorSpan(Color.BLUE);
+        ForegroundColorSpan fcsBlue2 = new ForegroundColorSpan(Color.BLUE);
+
+        itemCountDisplayString.setSpan(fcsBlue1, itemCountStartIndex, itemCountEndIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        itemCountDisplayString.setSpan(fcsBlue2, requestKeywordsStartIndex, requestKeywordsEndIndex, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        return itemCountDisplayString;
+    }
+
 }
