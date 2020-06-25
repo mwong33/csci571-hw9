7473daecadcd9a6009a6f05df9178de2e301b99f
Matthew James Wong <matthew.wong76@gmail.com>
2020-06-25 15:25:41 -0600

    Adding the skeleton of the getSingleItem request

--- app/src/main/java/com/example/ebaycatalogsearch/SingleItem.java
+++ app/src/main/java/com/example/ebaycatalogsearch/SingleItem.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SingleItem extends AppCompatActivity {
 
     private String productID;
     private String title;
+
+    // Request Object
+    private RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +38,32 @@ public class SingleItem extends AppCompatActivity {
         // Set the Single Item Title
         this.setTitle(title);
 
+        // Initialize the requestQueue
+        requestQueue = Volley.newRequestQueue(this);
+
+        // Make the getSingleItem request
+        getSingleItem();
+
+    }
+
+    private void getSingleItem() {
+
+        String requestUrl = "https://mjwong-csci-571-hw8.wl.r.appspot.com/singleItem?productID=" + productID;
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, requestUrl, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(request);
     }
 }
