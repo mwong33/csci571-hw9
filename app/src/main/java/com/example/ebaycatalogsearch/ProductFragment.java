909d1eeae294fdce6ac24ac783fd7fb3a246d28e
Matthew James Wong <matthew.wong76@gmail.com>
2020-06-26 12:56:44 -0600

    use onViewCreated to access elements for fragments

--- app/src/main/java/com/example/ebaycatalogsearch/ProductFragment.java
+++ app/src/main/java/com/example/ebaycatalogsearch/ProductFragment.java
@@ -4,17 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class ProductFragment extends Fragment {
+
+    private String itemString;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.product_fragment, container, false);
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // Initialize the itemString from the itemString passed by SingleItem activity
+        itemString = getArguments().getString("itemString");
+
+        TextView textView = getView().findViewById(R.id.textView);
+        textView.setText(itemString);
     }
 }
