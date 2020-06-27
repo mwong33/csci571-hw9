693f3a0ed1b3d9c5c25494b1582a927dc0ebcb5f
Matthew James Wong <matthew.wong76@gmail.com>
2020-06-26 18:06:12 -0600

    Adding seller info

--- app/src/main/java/com/example/ebaycatalogsearch/SellerInfoFragment.java
+++ app/src/main/java/com/example/ebaycatalogsearch/SellerInfoFragment.java
@@ -1,20 +1,24 @@
 package com.example.ebaycatalogsearch;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Iterator;
 
 public class SellerInfoFragment extends Fragment {
 
     private String itemString;
-    private String itemAdvancedString;
 
     @Nullable
     @Override
@@ -27,8 +31,57 @@ public class SellerInfoFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Initialize the itemString from the itemString passed by SingleItem activity
         itemString = getArguments().getString("itemString");
-        itemAdvancedString = getArguments().getString("itemAdvancedString");
+
+        try {
+            createSellerInformation();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
 
+    }
+
+    private void createSellerInformation() throws JSONException {
+        // Create the JSONObject
+        JSONObject item = new JSONObject(itemString);
+
+        // Set the sellerInformationTitle
+        String sellerInformationTitleHTML = "<p style=\"color:black\"><b>Seller Information</b></p>";
+        TextView sellerInformationTitle = getView().findViewById(R.id.sellerInformationTitle);
+        sellerInformationTitle.setText(Html.fromHtml(sellerInformationTitleHTML));
+
+        // Set the sellerBulletList
+        JSONObject seller = item.getJSONObject("Seller");
+        TextView sellerBulletList = getView().findViewById(R.id.sellerBulletList);
+        sellerBulletList.setText(Html.fromHtml(createBulletList(seller)));
+
+    }
+
+    private String createBulletList(JSONObject json) throws JSONException{
+        String bulletListHTML = "<ul>";
+
+        Iterator<String> keys = json.keys();
+
+        while(keys.hasNext()) {
+            String key = keys.next();
+            String value = json.getString(key);
+            String bulletEntry = String.format("<li><b>%s: </b>%s</li>", convertToReadable(key), value);
+            bulletListHTML += bulletEntry;
+        }
+
+        bulletListHTML += "</ul>";
+
+        return bulletListHTML;
+    }
+
+    private String convertToReadable(String camelCase) {
+
+        String[] splitCamelCaseArray = camelCase.split("(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])");
+
+        String readableString = String.join(" ", splitCamelCaseArray);
+
+        readableString = readableString.substring(0,1).toUpperCase() + readableString.substring(1);
+
+        return readableString;
     }
 }
