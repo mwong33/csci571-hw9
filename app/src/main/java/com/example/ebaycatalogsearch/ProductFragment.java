be2b5f8b5cc8692c005ae2e4596d7395f86a53cc
Matthew James Wong <matthew.wong76@gmail.com>
2020-06-26 22:17:51 -0600

    adding image scroll

--- app/src/main/java/com/example/ebaycatalogsearch/ProductFragment.java
+++ app/src/main/java/com/example/ebaycatalogsearch/ProductFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -31,7 +36,34 @@ public class ProductFragment extends Fragment {
         itemString = getArguments().getString("itemString");
         itemAdvancedString = getArguments().getString("itemAdvancedString");
 
-        TextView textView = getView().findViewById(R.id.textView);
-        textView.setText(itemString);
+        // Set the imageLinearView
+        try {
+            displayImageLinearView();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void displayImageLinearView() throws JSONException {
+
+        LinearLayout imageLinearView = getView().findViewById(R.id.imageLinearView);
+
+        JSONObject item = new JSONObject(itemString);
+        JSONArray pictureURLArray = item.getJSONArray("PictureURL");
+
+        for (int i = 0; i < pictureURLArray.length(); i++) {
+
+            String pictureURL = pictureURLArray.getString(i);
+
+            ImageView pictureView= new ImageView(getContext());
+            pictureView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT));
+            pictureView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+            Picasso.with(getContext()).load(pictureURL).into(pictureView);
+            imageLinearView.addView(pictureView);
+        }
+
     }
 }
